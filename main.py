6f5b7476a9e0406eaa809a432ecceaa589fbb8d4
TonyAlosius <tony.aloysius.77@gmail.com>
2023-06-26 15:39:26 +0530

    Preparing Base Model Completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.cnnCloud import logger
 from src.cnnCloud.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnCloud.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,3 +14,15 @@ except Exception as e:
     raise e
 
 # logger.info("Welcome to my custom log")
+
+
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
